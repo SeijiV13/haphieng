3167a300c7560c0b5aa7dfd7a6af13db3984c695
seiji <villafrancasra@gmail.com>
2018-05-27 16:20:32 +0800

    added dropdown values

--- src/app/views/reports/report-fields/field-generator/field-generator.component.ts
+++ src/app/views/reports/report-fields/field-generator/field-generator.component.ts
@@ -1,6 +1,10 @@
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import * as $ from 'jquery';
+import { CustomerService } from '../../../../web-services/customer.service';
+import { SupplierService } from '../../../../web-services/supplier.service';
+import { ProductsService } from '../../../../web-services/products.service';
+import { AgentService } from '../../../../web-services/agent.service';
 @Component({
   selector: 'app-field-generator',
   templateUrl: './field-generator.component.html',
@@ -13,8 +17,18 @@ export class FieldGeneratorComponent implements OnInit {
   @Input('fields') fields: Array<any> = [];
   @Output('generateReport') generateReport = new EventEmitter();
   formGroup: FormGroup;
-  constructor(private fb: FormBuilder) { }
-
+  constructor(private fb: FormBuilder, 
+    private customerService: CustomerService, 
+    private supplierService: SupplierService,
+    private productService: ProductsService,
+    private agentService: AgentService) {
+      this.getDropdownValues();
+     }
+  customers: Array<any>;
+  suppliers: Array<any>;
+  items: Array<any>;
+  categories: Array<any> = [];
+  agents: Array<any>;
   ngOnInit() {
     this.formGroup = this.fb.group({
       customer: [''],
@@ -26,6 +40,22 @@ export class FieldGeneratorComponent implements OnInit {
       date: [''],
       supplier: ['']
     })
+  }
+
+  getDropdownValues(){
+    this.customerService.getCustomers().subscribe((data)=>{
+      this.customers = data;
+    })
+    this.productService.getProducts().subscribe((data)=>{
+      this.items = data;
+    })
+    this.supplierService.getSuppliers().subscribe((data)=>{
+      this.suppliers = data;
+    });
+    this.agentService.getAgents().subscribe((data)=>{
+      this.agents = data;
+    })
+
   }
 
   clear(){
